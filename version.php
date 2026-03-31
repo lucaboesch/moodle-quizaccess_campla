024cd29bdd00711df5cb7dc6241eacb53b8ad02f
Luca Bösch <luca.boesch@bfh.ch>
2026-03-31 23:28:35 +0200

    v5.2-r1 release.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release = 'v5.1-r1';
-$plugin->version = 2025100600;
+$plugin->release = 'v5.2-r1';
+$plugin->version = 2026033100;
 $plugin->requires = 2024042200;
 $plugin->component = 'quizaccess_campla';
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->supported = [404, 501];
+$plugin->supported = [404, 502];
